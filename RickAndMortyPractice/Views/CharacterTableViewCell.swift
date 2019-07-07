950861762d856bbca6470a3a1e21cef2f6d9e663
Dustin Koch <dustin.koch@me.com>
2019-07-07 13:20:40 -0600

    completes initial storyboard and character cell outlets

---
+++ RickAndMortyPractice/Views/CharacterTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CharacterTableViewCell.swift
+//  RickAndMortyPractice
+//
+//  Created by Dustin Koch on 7/7/19.
+//  Copyright © 2019 Rabbit Hole Fashion. All rights reserved.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var characterNameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var originLabel: UILabel!
+    @IBOutlet weak var characterPictureImageView: UIImageView!
+    
+}//END OF CLASS
