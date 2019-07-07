72a59fa560a83db5f7429a6e492f3ceefe3149e8
Dustin Koch <dustin.koch@me.com>
2019-07-07 13:58:21 -0600

    finishes character model

---
+++ RickAndMortyPractice/Model Controllers/CharacterController.swift
@@ -0,0 +1,16 @@
+//
+//  CharacterController.swift
+//  RickAndMortyPractice
+//
+//  Created by Dustin Koch on 7/7/19.
+//  Copyright © 2019 Rabbit Hole Fashion. All rights reserved.
+//
+
+import Foundation
+
+class CharacterController {
+    
+    
+    
+    
+} // END OF CLASS
---
+++ RickAndMortyPractice/Models/Character.swift
@@ -0,0 +1,41 @@
+//
+//  Character.swift
+//  RickAndMortyPractice
+//
+//  Created by Dustin Koch on 7/7/19.
+//  Copyright © 2019 Rabbit Hole Fashion. All rights reserved.
+//
+
+import Foundation
+
+struct Character: Decodable {
+    
+    let name: String
+    let status: String
+    let origin: Origin
+    let image: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case status
+        case origin
+        case image
+    }
+    
+}//END OF STRUCT
+
+struct TopLevelDictionary: Decodable {
+    
+    let results = [Character]
+    
+}//END OF STRUCT
+
+struct Origin: Decodable {
+    
+    let planetName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case planetName = "name"
+    }
+    
+}//END OF STRUCT
